7b4a979e5c5dcb3a6f63a8077e81fcd82b0f0278
nvrqt03 <ajktg21@gmail.com>
2020-12-10 13:13:42 -0600

    created additional adapters, starting work on the detail activity

--- app/src/main/java/ajmitchell/android/bakingtime/RecipeDetailActivity.java
+++ app/src/main/java/ajmitchell/android/bakingtime/RecipeDetailActivity.java
@@ -1,14 +1,31 @@
 package ajmitchell.android.bakingtime;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import ajmitchell.android.bakingtime.databinding.ActivityMainBinding;
+import ajmitchell.android.bakingtime.databinding.RecipeItemBinding;
+
 public class RecipeDetailActivity extends AppCompatActivity {
+
+    ActivityMainBinding mBinding;
+    public static final String TAG = "RecipeDetailActivity.class";
+    private RecyclerView ingredientRecyclerView;
+    private RecyclerView stepRecyclerView;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_recipe_detail);
+
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_recipe_detail);
+        stepRecyclerView = mBinding.steps_rv;
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        stepRecyclerView.setLayoutManager(layoutManager);
     }
 }
---
+++ app/src/main/java/ajmitchell/android/bakingtime/adapters/StepAdapter.java
@@ -0,0 +1,63 @@
+package ajmitchell.android.bakingtime.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import ajmitchell.android.bakingtime.databinding.IngredientItemBinding;
+import ajmitchell.android.bakingtime.databinding.StepItemBinding;
+import ajmitchell.android.bakingtime.models.Ingredient;
+import ajmitchell.android.bakingtime.models.Step;
+
+public class StepAdapter extends RecyclerView.Adapter<StepAdapter.ViewHolder> {
+
+    private List<Step> stepList;
+
+    public StepAdapter(List<Step> stepList) {
+        this.stepList = stepList;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+        StepItemBinding stepItemBinding = StepItemBinding.inflate(inflater, parent, false);
+        StepAdapter.ViewHolder viewHolder = new StepAdapter.ViewHolder(stepItemBinding);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Step stepResults = stepList.get(position);
+        holder.bind(stepResults);
+    }
+
+    @Override
+    public int getItemCount() {
+        if (stepList == null){
+            return 0;
+        }
+        return stepList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        private StepItemBinding binding;
+
+        public ViewHolder(@NonNull StepItemBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+        }
+
+        public void bind(Step step) {
+            binding.setStep(step);
+            binding.executePendingBindings();
+        }
+    }
+}
